15c42c33ce84c02a19c1d8781dde78aa98a4ccd2
Sitthi Thiammekha <sitthi@mekha.ai>
2025-11-26 10:39:50 +0700

    add line login to save reporter info

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LiffProvider } from "./contexts/LiffContext";
 import Input from "./pages/Input";
 import SelectReports from "./pages/SelectReports";
 import Review from "./pages/Review";
@@ -15,29 +16,31 @@ import Footer from "./components/Footer";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<Input />} />
-              <Route path="/select" element={<SelectReports />} />
-              <Route path="/review" element={<Review />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/report/:id" element={<ReportDetail />} />
-              <Route path="/help" element={<Help />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+  <LiffProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<Input />} />
+                <Route path="/select" element={<SelectReports />} />
+                <Route path="/review" element={<Review />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/report/:id" element={<ReportDetail />} />
+                <Route path="/help" element={<Help />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </LiffProvider>
 );
 
 export default App;
